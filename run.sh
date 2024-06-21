73bd9fdac99c94b2e9a567ea6ab52231c7125c2d
SJWarrior-17 <onlyswapniljain@gmail.com>
2024-06-21 19:04:05 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum --disablerepo=kubernetes
+yum-config-manager --disable kubernetes
+echo "Docker installation"
+echo -e "\n"
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+echo -e "\n"
+echo "Testing Docker"
+docker version
+
+helm_install() {
+    chmod 777 ./helm/install.sh
+    cd helm
+    . "./install.sh"  
+}
+
+helm_install
