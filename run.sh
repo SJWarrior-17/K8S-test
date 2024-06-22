e85e538ca4de3b5a27269efacd05664f46a24b2c
SJWarrior-17 <onlyswapniljain@gmail.com>
2024-06-22 16:15:21 +0530

    Update the scriprt

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+echo "Deploying K8S cluster."
+echo -e "\n"
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+sleep 20
+echo "Please copy join code"
+echo -e "\n"
+echo "Installing Cluster networking."
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+echo -e "\n"
+echo "Updating Yum package managaer."
 yum --disablerepo=kubernetes
 yum-config-manager --disable kubernetes
 echo "Docker installation"
